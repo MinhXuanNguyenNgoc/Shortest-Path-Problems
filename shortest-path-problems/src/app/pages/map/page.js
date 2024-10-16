bc526030b93a450687da978805e6eb442eb878ad
MinhXuanNguyenNgoc <80444653+MinhXuanNguyenNgoc@users.noreply.github.com>
2024-10-16 17:31:18 +0700

    update for homepage and map page

---
+++ shortest-path-problems/src/app/pages/map/page.js
@@ -0,0 +1,49 @@
+"use client";
+
+import React from "react";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+
+export default function Mapdisplay() {
+  const containerStyle = {
+    width: "100%",
+    height: "90vh",
+  };
+
+  const center = {
+    lat: 10.773240893415258,
+    lng: 106.6572732015988,
+  };
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyBI2x6WXz-BycuLkH5O_DJBlS0LuLFKMy8",
+  });
+
+  const [map, setMap] = React.useState(null);
+
+  const onLoad = React.useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={20}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
+  );
+}
