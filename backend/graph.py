3353f4938b8661ba982d3ae481f927be032bd7d0
Felix <xdcobra11@gmail.com>
2024-10-30 17:49:50 +0700

    Minimum spanning tree

---
+++ backend/graph.py
@@ -0,0 +1,47 @@
+# This file contains function to manage the graph theory
+
+import heapq
+from OSM import calculate_distance
+
+def create_minimum_spanning_tree(nodes):
+    """
+    Creates a minimum spanning tree using Prim's Algortihm
+    
+    :param nodes: A list of coordinates e.g. [(106.5455434,10.4554545),(...)]
+
+    :return result: An Array containing the nodes and its edges in this format [(start_node,end_node,distance)] e.g. [((106.5455434,10.4554545),(106.5455454,10.4554598),210)]
+    """
+    num_nodes = len(nodes)
+    if num_nodes == 0:
+        return []
+
+    # Min-Heap für die Kanten
+    min_heap = []
+    visited = [False] * num_nodes
+    result = []
+
+    # Starte mit dem ersten Knoten
+    visited[0] = True
+    edges_count = 0  # Zähler für die Anzahl der Kanten
+
+    # Füge alle Kanten vom ersten Knoten zu den anderen Knoten hinzu
+    for j in range(1, num_nodes):
+        distance = calculate_distance(nodes[0], nodes[j])
+        heapq.heappush(min_heap, (distance, 0, j))  # (Distanz, von, nach)
+
+    while min_heap and edges_count < num_nodes - 1:
+        distance, from_node, to_node = heapq.heappop(min_heap)
+        if not visited[to_node]:
+            visited[to_node] = True
+            starting_node = (nodes[from_node][0], nodes[from_node][1])
+            ending_node = (nodes[to_node][0], nodes[to_node][1])
+            result.append((starting_node,ending_node, distance))
+            edges_count += 1
+
+            # Füge neue Kanten von dem neu besuchten Knoten hinzu
+            for j in range(num_nodes):
+                if not visited[j]:
+                    new_distance = calculate_distance(nodes[to_node], nodes[j])
+                    heapq.heappush(min_heap, (new_distance, to_node, j))
+
+    return result
